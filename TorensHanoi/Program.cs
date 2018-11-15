85ef1e1b081549fb31d815c7e906645d862dd8c8
Huego <hugozaanen@hotmail.com>
2018-11-15 14:20:36 +0100

    Torents

--- TorensHanoi/Program.cs
+++ TorensHanoi/Program.cs
@@ -10,18 +10,30 @@ namespace TorensHanoi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kies een grote");
             int size = Int32.Parse(Console.ReadLine());
+            int i = 0; 
 
             DiskCollection game = new DiskCollection(size);
             Disk[] toren2 = new Disk[size];
             Disk[] toren3 = new Disk[size];
+            
 
             bool play = true;
 
             while(play)
             {
-
+                Console.WriteLine("Kies stapel 1, 2 of 3");
+                
+                int choice = Int32.Parse(Console.ReadLine());
+                i = Int32.Parse(Console.ReadLine());
+                if (i == 1)
+                {
+                    play = false;
+                }
             }
+
+            Console.Read();
         }
     }
 }
